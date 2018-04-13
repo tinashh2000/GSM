5dd02517ff845357202e7c3a17f9b1a50450daed
mtdevices <tinashh2000@gmail.com>
2018-04-14 00:43:04 +0200

    commit

--- Program.cs
+++ Program.cs
@@ -1,4 +1,19 @@
-﻿using System;
+﻿/***************************************************************************************
+* Copyright (C) 2018 Tinashe Mutandagayi                                               *
+*                                                                                      *
+* This file is part of the GSM System source code. The author(s) of this file          *
+* is/are not liable for any damages, loss or loss of information, deaths, sicknesses   *
+* or other bad things resulting from use of this file or software, either direct or    *
+* indirect.                                                                            *
+* Terms and conditions for use and distribution can be found in the license file named *
+* LICENSE.TXT. If you distribute this file or continue using it,                       *
+* it means you understand and agree with the terms and conditions in the license file, *
+* if not, do not use the file														   *
+*                                                                                      *
+* Happy Coding :)                                                                      *
+****************************************************************************************/
+
+using System;
 using System.Collections.Generic;
 //using System.Linq;
 using System.Windows.Forms;
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# gsm
+The source code in this project can be freely used according to the license.txt as long as the copyright notice is kept in place. Altering the copyright notice violates the terms of use for the files in this project
